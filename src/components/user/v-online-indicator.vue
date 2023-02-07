845ad37c29922514bf6ee8a33e17605d62f82239
emiljs777 <e.pargevich@mail.ru>
2023-02-07 19:17:49 +0400

    online_indicator

--- src/components/user/v-online-indicator.vue
+++ src/components/user/v-online-indicator.vue
@@ -15,6 +15,9 @@ export default {
     this.sockets.subscribe('online', (data) => {
       data.user_id === this.user_id ? this.online = true : null
     })
+    this.sockets.subscribe('offline', (data) => {
+      data.user_id === this.user_id ? this.online = false : null
+    })
   },
   mounted() {
     this.$socket.emit('get_online', {user_id: this.user_id})
