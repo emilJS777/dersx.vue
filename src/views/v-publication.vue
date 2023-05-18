b2278bff365d0b9611b4f90abb0e11046751f1c7
emiljs777 <e.pargevich@mail.ru>
2023-05-18 14:34:35 +0400

    lang

--- src/views/v-publication.vue
+++ src/views/v-publication.vue
@@ -2,15 +2,20 @@
   <div class="publication d-grid g-gap-3">
       <div></div>
     <v-publication-list v-if="publication" :publication="publication"/>
+      <h3 class="t-center c-ccc m-top-0" v-else>{{ lang.general.nothing_found }}</h3>
   </div>
 </template>
 
 <script>
 import VPublicationList from "@/components/publication/v-publication-list.vue";
+import {mapState} from "vuex";
 
 export default {
     name: "v-publication",
     components: {VPublicationList},
+    computed: mapState({
+        lang: state => state.lang.LANG
+    }),
     data(){
         return{
             publication: null
